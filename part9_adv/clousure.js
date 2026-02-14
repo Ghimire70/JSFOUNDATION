293cddadd8fb41fd2c4053ce014edb463523350e
[Utsav Ghimire] <[utsavghimire34@gmail.com]>
2026-02-14 17:32:18 +0545

    feat: Add closure, explore, and promises examples in separate files

---
+++ part9_adv/clousure.js
@@ -0,0 +1,15 @@
+//Clousure is basically functions that remember the environment in which
+//they where created
+
+function outer() {
+  let counter = 4;
+  return function () {
+    counter++;
+    return counter;
+  };
+}
+
+let increment = outer();
+console.log(increment());
+console.log(increment());
+console.log(increment());
---
+++ part9_adv/explore1.js
@@ -0,0 +1,14 @@
+
+function sayHello() {
+    console.log("I would like to say hello!");
+}
+
+setTimeout(() => {
+    sayHello();
+}, 5000);
+
+console.log("myCode");
+
+for (let index = 0; index < 10; index++) {
+  console.log(index);
+}
