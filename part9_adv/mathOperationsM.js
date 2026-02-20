1444d30faee3b4e78b9bf89f94cc329c1aaca308
[Utsav Ghimire] <[utsavghimire34@gmail.com]>
2026-02-20 20:21:43 +0545

    feat: Add math operations module with add, subtract, and multiply functions; create appC.js and appM.js for testing

---
+++ part9_adv/mathOperationsM.js
@@ -0,0 +1,13 @@
+//es6 modules
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
+
+export default function multiply(a, b) {
+    return a*b;
+}
