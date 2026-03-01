ed2ee83cf872c2a57080bb201ade5d400412271a
[Utsav Ghimire] <[utsavghimire34@gmail.com]>
2026-03-01 22:34:20 +0545

    commit

--- tasks/task1.js
+++ tasks/task1.js
@@ -2,23 +2,54 @@
 // Create a function simulateAsyncTask() that logs “Task started”, then after 2 seconds logs “Task finished”.
 // Use setTimeout to simulate this behaviour.
 
-function simulateAsyncTask() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({ name: "Utsav", my_website: "/https://utsavdada.com" });
-    }, 2000);
-  });
+// Task 2: Simulate Multiple Async Tasks with Different Delays
+// Create a function simulateMultipleTasks() that starts three asynchronous tasks with different delays (1 second, 2 seconds, and 3 seconds).
+// Each task should log "Task [n] finished" where [n] is the task number. Ensure the tasks run asynchronously.
+
+// Task 3: Async Task with Callback Function
+// Create a function fetchDataWithCallback(callback) that simulates fetching data asynchronously using setTimeout (after 2 seconds).
+// Once the data is “fetched”, it should invoke the provided callback function with "Fetched data" as an argument.
+
+//------starts here-------//
+
+//task1
+// function simulateAsyncTask() {
+//   console.log("Task started"); // runs immediately
+//   setTimeout(() => {
+//     console.log("Task finished"); // runs after 2 seconds
+//   }, 2000);
+// }
+// simulateAsyncTask();
+
+//task2
+
+// function simulateMultipleTasks() {
+//   console.log("All tasks started");
+
+//   setTimeout(() => {
+//     console.log("process 1 completed");
+//   }, 1000);
+
+//   setTimeout(() => {
+//     console.log("process 2 completed");
+//   }, 2000);
+
+//   setTimeout(() => {
+//     console.log("process 3 completed");
+//   }, 3000);
+// }
+// simulateMultipleTasks();
+
+//task 3
+
+function fetchDataWithCallback(callback) {
+  console.log("fetching data...");
+  setTimeout(() => {
+    const data = "Fetched Data";
+    callback(data);
+  }, 2000);
 }
 
-async function fetchUserData() {
-  try {
-    console.log("Fetching your data...(2 seconds)");
-    const yourData = await simulateAsyncTask();
-    console.log("sucessfully fetched your data in the system!");
-
-    console.log("Your data:", yourData);
-  } catch (error) {
-    console.log("Error fetching your data", error);
-  }
-}
-fetchUserData();
+fetchDataWithCallback((data) => {
+  console.log("Received:", data);
+});
