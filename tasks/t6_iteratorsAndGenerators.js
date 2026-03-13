ec6256cbf996c699342f003b3699e579eb89c343
[Utsav Ghimire] <[utsavghimire34@gmail.com]>
2026-03-13 19:36:04 +0545

    c

--- tasks/t6_iteratorsAndGenerators.js
+++ tasks/t6_iteratorsAndGenerators.js
@@ -4,7 +4,8 @@
 // Create a custom iterator called rangeIterator(start, end) that returns an object that iterates over numbers from start to end.
 // Each call to .next() should return the next number in the range until it reaches end.
 // Task 3: Generator Function for Fibonacci Sequence
-// Create a generator function fibonacciGenerator() that yields numbers from the Fibonacci sequence indefinitely (1, 1, 2, 3, 5, 8, etc.).
+// Create a generator function fibonacciGenerator() that yields numbers from the Fibonacci 
+// sequence indefinitely (1, 1, 2, 3, 5, 8, etc.).
 // Use the next() method to get the next Fibonacci number.
 
 /*
